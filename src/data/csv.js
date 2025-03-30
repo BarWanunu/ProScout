0ed3becbd66474a9ec10613defecdb7c3b7524fe
BarWanunu <barwanunu6@gmail.com>
2025-03-30 11:51:46 +0300

    Half Ligue1 player stats added + creating CSV files for teams and players to export to the DB

---
+++ src/data/csv.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import { Parser } from "json2csv";
+
+const baseFolder = "./Players";
+const leagueFolders = fs
+  .readdirSync(baseFolder)
+  .filter((folder) =>
+    fs.lstatSync(path.join(baseFolder, folder)).isDirectory()
+  );
+
+const allPlayers = [];
+
+for (const leagueFolder of leagueFolders) {
+  const leaguePath = path.join(baseFolder, leagueFolder);
+  const files = fs
+    .readdirSync(leaguePath)
+    .filter((file) => file.endsWith(".json"));
+
+  for (const file of files) {
+    const filePath = path.join(leaguePath, file);
+    const playerArray = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    allPlayers.push(...playerArray);
+  }
+}
+
+const fields = [
+  "id",
+  "name",
+  "firstname",
+  "lastname",
+  "number",
+  "club",
+  "position",
+  "height",
+  "weight",
+  "nationality",
+  "birthdate",
+  "photo",
+];
+
+const parser = new Parser({ fields });
+const csv = parser.parse(allPlayers);
+
+fs.writeFileSync("all_players.csv", csv, "utf-8");
+console.log("✅ CSV created: all_players.csv");
